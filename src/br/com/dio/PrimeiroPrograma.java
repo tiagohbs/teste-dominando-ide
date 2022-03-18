66767ef8c60a8b808eb73bf9ace9e8f68b843e5d
Borges <tiagoborges2940@gmail.com>
2022-03-18 09:47:49 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	
+	
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		System.out.println(gato);
+		System.out.println();
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello, world " + (a+b));*/
+
+	}
+class Livros {
+}
+}
